991918d798baf0d4147bb1184490ab72b50c4b11
icika-max <icika-max>
2022-11-18 12:49:08 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  a+b
+
+}
+
+function subtract(a,b) {
+  a-b
+}
+function multiply(a,b) {
+  a*b
+}
+function divide(a,b) {
+  a/b
+}
+function add(a,b) {
+return a+b
+}
+function subtract(a,b) {
+  return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function increment(n){
+  return n+1
+}
+function decrement(n){
+  return n-1
+}
+function makeInt(n=10.08) {
+return parseInt(n)
+
+}
+function preserveDecimal(n=10.99){
+  return parseFloat(n)
+}
